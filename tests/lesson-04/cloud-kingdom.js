0ec1b1de9f16570c1489804cf9a3a0e623853708
Nguyen Thuy Duong <duongthuynguyen00@gmail.com>
2026-07-26 22:29:58 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,11 @@
+let playerName = "Mario";
+let currentLives = 3;
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+
+let total = (level1+level2+level3);
+let avr = (total/3);
+let num = total%3;
+
+console.log(num)
